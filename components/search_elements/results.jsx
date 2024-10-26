06ef1ba9904d7edf74b894fc78c19093bdb9cd98
angrezichatterbox <gouthammohanraj@gmail.com>
2024-10-26 18:26:55 +0530

    feat:Added tags

--- components/search_elements/results.jsx
+++ components/search_elements/results.jsx
@@ -2,44 +2,76 @@ import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 
-export default function SampleResults({open, setOpen, setCurrIndex, data, currIndex,  setFocusPos}){
-    const handleShowBrief=(index, fruit)=>{
+export default function SampleResults({
+  open,
+  setOpen,
+  setCurrIndex,
+  data,
+  currIndex,
+  setFocusPos,
+}) {
+  const tags = ["Alpha", "Beta", "Gamma", "Delta", "Sale"];
 
-        if (open && currIndex!=undefined && index==currIndex){
-            setOpen(false)
-        }else{
-            setOpen(true)
-            setCurrIndex(index)
-            setFocusPos(fruit.data.location.coordinates)
-        }
-        console.log("clicked brief open", open, index, fruit)
+  const handleShowBrief = (index, fruit) => {
+    if (open && currIndex !== undefined && index === currIndex) {
+      setOpen(false);
+    } else {
+      setOpen(true);
+      setCurrIndex(index);
+      setFocusPos(fruit.data.location.coordinates);
     }
-    return(
-        <div>
-            <ul className="text-black mt-[2vw]">
-                {data.map((fruit, index) => (
-                    <li>
-                    <button key={index} className="min-h-[8vw] flex items-center w-full" onClick={()=>{handleShowBrief(index, fruit)}}>
-                        <div className="max-h-[7.5vw] overflow-hidden rounded-[0.5vw]">
-                            <Image src={fruit.image_url} width={5} height={5} className="h-auto min-w-[7.5vw]" unoptimized/>
-                        </div>
-                        <div className="flex flex-col min-h-[7.5vw] flex-1 mx-[1vw]">
-                            <div className="text-start flex-1 ">
-                                <p className="text-[1.5vw] leading-[1.5vw]">
-                                    {fruit.data.address.city}, {fruit.data.address.county}, {fruit.data.address.state_district}, {fruit.data.address.country}, {fruit.data.address.postcode}
-                                </p>
-                                <p>54</p>
-                            </div>
-                            <div className="flex-1 flex items-center">
-                                <p className="text-[2vw] font-semibold">{fruit.price}</p>
-                            </div>
-                        </div>
-                    </button>
-                    <div className="w-full h-0.5 bg-black rounded-[1vw] my-[1vw]"></div>
-                    </li>
-                ))
-                }
-            </ul>
-        </div>
-    )
+    console.log("clicked brief open", open, index, fruit);
+  };
+
+  return (
+    <div>
+      <div className="flex flex-wrap gap-2 mb-4 mt-4">
+        {tags.map((tag, index) => (
+          <span
+            key={index}
+            className="px-3 py-1 rounded-full text-sm transition duration-300 ease-in-out bg-[#D2B48C] text-white hover:bg-[#5B3A29]"
+          >
+            {tag}
+          </span>
+        ))}
+      </div>
+
+      <ul className="text-black mt-[2vw]">
+        {data.map((fruit, index) => (
+          <li key={index}>
+            <button
+              className="min-h-[8vw] flex items-center w-full"
+              onClick={() => handleShowBrief(index, fruit)}
+            >
+              <div className="max-h-[7.5vw] overflow-hidden rounded-[0.5vw]">
+                <Image
+                  src={
+                    "https://media.istockphoto.com/id/1029797404/photo/cheenavala-in-kochi.jpg?s=612x612&w=0&k=20&c=njgmKK-rdoZS0BWz0MkZuWJHzpE0qzwKuqtpsBz-BB0="
+                  }
+                  width={5}
+                  height={5}
+                  className="h-auto min-w-[7.5vw]"
+                  unoptimized
+                />
+              </div>
+              <div className="flex flex-col min-h-[7.5vw] flex-1 mx-[1vw]">
+                <div className="text-start flex-1">
+                  <p className="text-[1.5vw] leading-[1.5vw]">
+                    {fruit.data.address.city}, {fruit.data.address.county},{" "}
+                    {fruit.data.address.state_district},{" "}
+                    {fruit.data.address.country}, {fruit.data.address.postcode}
+                  </p>
+                  <p>54</p>
+                </div>
+                <div className="flex-1 flex items-center">
+                  <p className="text-[2vw] font-semibold">{fruit.price}</p>
+                </div>
+              </div>
+            </button>
+            <div className="w-full h-0.5 bg-black rounded-[1vw] my-[1vw]"></div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
